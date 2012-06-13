9586169d91aaf376bc765fcf980c59ffea570276
Josh Suereth <joshua.suereth@gmail.com>
2012-06-13 15:39:52 -0400

    Added more robust metadata print/reparse test.

--- distributed/projects/src/test/scala/distributed/project/dependencies/ParserSpec.scala
+++ distributed/projects/src/test/scala/distributed/project/dependencies/ParserSpec.scala
@@ -20,5 +20,20 @@ object ParserSpec extends Specification {
     }]
 }""") must equalTo(Some(ExtractedBuildMeta("foo/bar", Seq(Project("p1", "o1", Seq.empty)))))
     }
+    
+    "parse pretty printed metadata" in {
+      val data = 
+        ExtractedBuildMeta("foo/bar", 
+          Seq(
+              Project("p1", "o1", Seq.empty),
+              Project("p2", "o1", Seq(
+                  ProjectDep("p3", "o2"),
+                  ProjectDep("p4", "o3")
+                  ))))
+      val config = pretty.PrettyPrint(data)
+      (ExtractedDependencyFileParser.parseMetaString(config) 
+          must 
+          equalTo(Some(data)))
+    }
   }
 }

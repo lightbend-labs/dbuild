996aa0aeef87c1fe962f2cc494e9711215f2c992
Josh Suereth <joshua.suereth@gmail.com>
2012-06-22 12:48:54 -0400

    Fix to hashing code.

--- hashing/src/main/scala/hashing/package.scala
+++ hashing/src/main/scala/hashing/package.scala
@@ -21,12 +21,12 @@ package object hashing {
     val buf = new StringBuffer
     def byteToHex(b: Int) =
       if ((0 <= b) && (b <= 9)) ('0' + b).toChar
-      else ('a' + b).toChar
+      else ('a' + (b-10)).toChar
     for (i <- 0 until data.length) {
       buf append byteToHex((data(i) >>> 4) & 0x0F)
       buf append byteToHex(data(i) & 0x0F)
     }
-    return buf.toString
+    buf.toString
   }
 
 }

fc038acd7b5dbeb5c1bf325d7e88be9c03b72155
Josh Suereth <joshua.suereth@gmail.com>
2012-06-28 09:16:48 -0400

    Adding SHA-512 just in case we have collision issues.

--- hashing/src/main/scala/hashing/package.scala
+++ hashing/src/main/scala/hashing/package.scala
@@ -1,7 +1,12 @@
 package object hashing {
 
-  def sha1Sum(t: Any): String = {
-    val md = java.security.MessageDigest.getInstance("SHA-1")
+  def sha1Sum(t: Any): String =
+    messageDigestHex(java.security.MessageDigest.getInstance("SHA-1"))(t)
+
+  def sha512(t: Any): String =
+    messageDigestHex(java.security.MessageDigest.getInstance("SHA-512"))(t)
+  
+  def messageDigestHex(md: java.security.MessageDigest)(t: Any): String = {
     def addBytes(obj: Any): Unit = obj match {
       case b: Byte            => md update b
       case bytes: Array[Byte] => md update bytes
@@ -11,12 +16,14 @@ package object hashing {
         md update 5.toByte
         s.productIterator foreach addBytes
       case s: Traversable[_] =>
+        // First add a traversable marker..
         md update 1.toByte
         s foreach addBytes
     }
     addBytes(t)
     convertToHex(md.digest)
   }
+  
   def convertToHex(data: Array[Byte]): String = {
     val buf = new StringBuffer
     def byteToHex(b: Int) =

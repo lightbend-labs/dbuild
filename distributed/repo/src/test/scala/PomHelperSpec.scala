4a391fd4fd4112d986dee03aa1826673a801d65e
Josh Suereth <joshua.suereth@gmail.com>
2012-07-03 22:26:07 -0400

    missed test from previous.

---
+++ distributed/repo/src/test/scala/PomHelperSpec.scala
@@ -0,0 +1,57 @@
+package distributed
+package repo
+
+import org.specs2.mutable.Specification
+import project.model._
+import org.specs2.analysis.Dependencies
+import java.io.File
+
+object PomHelperSpec extends Specification {
+  
+  def makeBuildArts: (DistributedBuild, BuildArtifacts) = {
+    val build = DistributedBuild(
+            Seq(Build(
+                config = BuildConfig("", "", "" ,""),
+                extracted = ExtractedBuildMeta(
+                  uri = "",
+                  projects = Seq(
+                    Project(
+                      name = "scala-arm",
+                      organization = "com.jsuereth",
+                      artifacts = Seq(ProjectDep("scala-arm", "com.jsuereth")),
+                      dependencies = Seq(ProjectDep("scala-library", "org.scala-lang"))
+                    )
+                  )
+                )
+            ))
+          )
+      val arts = BuildArtifacts(Seq(
+        ArtifactLocation(ProjectDep("scala-arm", "com.jsuereth"), new File("."), "1.2")    
+      ), new File("."))
+      
+    (build,arts)
+  }
+  
+  "A PomHelper" should {
+    "create pom models" in {
+      val (build,arts) = makeBuildArts
+      val poms = PomHelper.makePoms(build, arts)
+      
+      poms must haveSize(1)
+      val pom = poms.head
+      pom.getArtifactId must equalTo("scala-arm")
+      pom.getDependencies.size must equalTo(1)
+    }
+    "create pom strings" in {
+      val (build,arts) = makeBuildArts
+      val poms = PomHelper.makePomStrings(build, arts)
+      
+      poms must haveSize(1)
+      val pom = poms.head
+      println(pom)
+      pom must contain("<artifactId>scala-arm</artifactId>")
+      pom must contain("<dependencies>")
+      pom must contain("<dependency>")
+    }
+  }
+}

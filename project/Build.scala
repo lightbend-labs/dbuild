2a6902be5d33a969d0298e70c9eba59145d81502
Josh Suereth <joshua.suereth@gmail.com>
2012-08-06 14:58:01 -0400

    bump for future dev

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object DistributedBuilderBuild extends Build with BuildHelper {
 
   override def settings = super.settings ++ SbtSupport.buildSettings
 
-  def MyVersion: String = "0.2"
+  def MyVersion: String = "0.3-SNAPSHOT"
   
   lazy val root = (
     Project("root", file(".")) 

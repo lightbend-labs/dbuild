f13b65d8c1b33bf99031cb794981eaa2ba3dd068
Josh Suereth <joshua.suereth@gmail.com>
2012-08-14 12:21:01 -0400

    0.3 release

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object DistributedBuilderBuild extends Build with BuildHelper {
 
   override def settings = super.settings ++ SbtSupport.buildSettings
 
-  def MyVersion: String = "0.3-SNAPSHOT"
+  def MyVersion: String = "0.3"
   
   lazy val root = (
     Project("root", file(".")) 

4b59465b42484b469d99b21663357ca6e98a5a4a
Antonio Cunei <antonio.cunei@typesafe.com>
2013-03-14 18:20:00 +0100

    Refactoring (still in progress)...

--- distributed/repo/src/main/scala/distributed/repo/core/exceptons.scala
+++ distributed/repo/src/main/scala/distributed/repo/core/exceptons.scala
@@ -10,3 +10,5 @@ case class ResolveException(key: String, msg: String) extends java.lang.RuntimeE
 case class MalformedMetadata(key: String, msg: String) extends java.lang.RuntimeException(msg) with RepositoryException
 /** We had an issue attempting to store the artifact in the cache. */
 case class StoreException(key: String, msg: String) extends java.lang.RuntimeException(msg) with RepositoryException
+/** We were copying files from the local repo to a new Scala "lib" directory, but a needed file couldn't be copied/found */
+case class LocalArtifactMissingException(key: String, msg: String) extends java.lang.RuntimeException(msg) with RepositoryException

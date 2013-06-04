2e0e0155d1dd12f63b479d2036cd537b3f91a258
Antonio Cunei <antonio.cunei@typesafe.com>
2013-06-04 03:27:31 +0200

    Check dependencies within sbt subprojects
    When extracting dependencies from an sbt-based project,
    check whether any of the subprojects are required
    dependencies of some of the subprojects specified in
    the "extra" projects argument. If any exist that
    were not previously listed, add them and emit a
    friendly warning.
    
    Should fix #30.

--- distributed/support/sbt-plugin/src/main/scala/com/typesafe/dbuild/DependencyAnalysis.scala
+++ distributed/support/sbt-plugin/src/main/scala/com/typesafe/dbuild/DependencyAnalysis.scala
@@ -46,9 +46,44 @@ object DependencyAnalysis {
     val allRefs=getProjectRefs(session.mergeSettings)
     verifySubProjects(projects, allRefs)
     val refs = if (projects.isEmpty)
-        allRefs
-      else
-        allRefs.filter(isValidProject(projects, _))
+      allRefs
+    else {
+      val requested = allRefs.filter(isValidProject(projects, _))
+      // let's investigate whether any other local projects
+      // are dependencies of the requested ones
+      //
+      // TODO: code is too convoluted; can it be simplified a little?
+      val localModulesMap = allRefs map { r => (r.project -> extracted.get(Keys.projectID in r)) } toMap
+      val localModulesIDs = localModulesMap.values.toList
+      val localRefsMap = allRefs map { r => (r.project -> r) } toMap
+
+      def dependencies(toScan: Seq[String], scanned: Map[String, ModuleID]): Map[String, ModuleID] = {
+        toScan.toList match {
+          case project :: rest =>
+            val id = localModulesMap(project)
+            val Some(Value(deps)) = Project.evaluateTask(Keys.projectDependencies in localRefsMap(project), state)
+            def matc(id:ModuleID, d:ModuleID)=
+              id.name == d.name && id.organization == d.organization && id.revision == d.revision
+            val newDeps = deps filter {
+              d => localModulesIDs.exists { id => matc(id, d) } && !scanned.exists { case (_, id) => matc(id, d) }
+            }
+            // Find the local projects for newDeps:
+            val newDepsNames = newDeps map { d => val Some((n, _)) = localModulesMap.find { case (_, id) => matc(id,d) }; n }
+            val newRest=(rest ++ newDepsNames).distinct
+            val added=newRest diff rest
+            if (added.size!=0)
+              println("Adding these subprojects, as they are needed by "+project+" (and possibly by others): " + added.mkString(", "))
+            dependencies(newRest, scanned + (project -> id))
+          case _ => scanned
+        }
+      }
+      val newProjectNames = dependencies(projects, Map()) map (_._1)
+      if (newProjectNames.size != projects.size) {
+        println("\n*** WARNING ***\n\nFurther subprojects will be included, as they are dependencies of the requested projects.")
+        println((newProjectNames.toList diff projects).mkString("Added: ",", ",".\n"))
+      }
+      newProjectNames map localRefsMap
+    }
     val deps = getProjectInfos(extracted, state, refs)    
     val meta = model.ExtractedBuildMeta(uri, deps)
     val output = new java.io.PrintStream(new java.io.FileOutputStream(file))

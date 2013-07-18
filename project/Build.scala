97676d03d2c8c20fb7ea26144394a8774b5f0bfb
Antonio Cunei <antonio.cunei@typesafe.com>
2013-07-18 17:56:26 +0200

    Bump version to 0.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object DistributedBuilderBuild extends Build with BuildHelper {
 
   override def settings = super.settings ++ SbtSupport.buildSettings
 
-  def MyVersion: String = "0.6.0"
+  def MyVersion: String = "0.6.1"
   
   lazy val root = (
     Project("root", file(".")) 

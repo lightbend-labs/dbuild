2fc7336b118815e8420e0b2bf89ae28c0bb9a671
Antonio Cunei <antonio.cunei@typesafe.com>
2013-08-07 21:44:38 +0200

    More checking during serialization/deserialization

--- distributed/metadata/src/main/scala/distributed/project/model/Utils.scala
+++ distributed/metadata/src/main/scala/distributed/project/model/Utils.scala
@@ -12,30 +12,34 @@ import java.io.File
 object Utils {
   private val mapper=JacksMapper.withOptions(CaseClassCheckNulls(true),
       CaseClassSkipNulls(true),CaseClassRequireKnown(true))
-  private def readValueT[T](c:Config)(implicit m: Manifest[T]) = 
-    withContextLoader(getClass.getClassLoader){
-      val expanded=c.resolve.root.render(ConfigRenderOptions.concise)
+  private def readValueT[T](c: Config)(implicit m: Manifest[T]) =
+    withContextLoader(getClass.getClassLoader) {
+      val expanded = c.resolve.root.render(ConfigRenderOptions.concise)
       try {
         mapper.readValue[T](c.resolve.root.render(ConfigRenderOptions.concise))
       } catch {
-        case e:JsonMappingException =>
-          val margin=50
-          val len=expanded.length()
-          val offset=e.getLocation().getCharOffset().toInt
-          val (s1, o1)=if (offset>margin) {
-            ("..."+expanded.substring(offset-margin+3),margin)
-          } else (expanded,offset)
-          val l1=s1.length()
-          val s2=if (l1-o1>margin) {
-            s1.substring(0, o1+margin-3)+"..."
-          } else s1
-          val m1="\n"+s2+"\n"+" "*o1+"^"
-          val m2=if (e.getMessage().startsWith("Can not deserialize instance of java.lang.String"))
-            m1+"\nA string may have been found in place of an array, somewhere in this object" else m1
-          throw new JsonMappingException(e.getMessage.split("\n")(0)+m2,e.getCause)
+        case e: JsonMappingException =>
+          val m2 = try {
+            val margin = 50
+            val len = expanded.length()
+            val offset = e.getLocation().getCharOffset().toInt
+            val (s1, o1) = if (offset > margin) {
+              ("..." + expanded.substring(offset - margin + 3), margin)
+            } else (expanded, offset)
+            val l1 = s1.length()
+            val s2 = if (l1 - o1 > margin) {
+              s1.substring(0, o1 + margin - 3) + "..."
+            } else s1
+            val m1 = "\n" + s2 + "\n" + " " * o1 + "^"
+            if (e.getMessage().startsWith("Can not deserialize instance of java.lang.String"))
+              m1 + "\nA string may have been found in place of an array, somewhere in this object" else m1
+          } catch {
+            case f => throw new JsonMappingException("Internal dbuild exception while deserializing; please report.", e)
+          }
+          throw new JsonMappingException(e.getMessage.split("\n")(0) + m2, e.getCause)
       }
     }
-  def readValue[T](f:File)(implicit m: Manifest[T])=readValueT[T](parseFile(f))
+  def readValue[T](f: File)(implicit m: Manifest[T]) = readValueT[T](parseFile(f))
   def readValue[T](s:String)(implicit m: Manifest[T])=readValueT[T](parseString(s))
   def writeValue[T](t:T)(implicit m: Manifest[T]) = 
     withContextLoader(getClass.getClassLoader){mapper.writeValueAsString[T](t)}

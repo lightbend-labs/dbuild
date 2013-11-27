0e74c48634218914abba2bad916ac643ceaf1002
Antonio Cunei <antonio.cunei@typesafe.com>
2013-11-27 17:45:08 +0100

    Bumped sbt-pgp to 0.8.2 (thanks Josh!)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   val aws            = "com.amazonaws" % "aws-java-sdk" % "1.3.29"
   val uriutil        = "org.eclipse.equinox" % "org.eclipse.equinox.common" % "3.6.0.v20100503"
 
-  val gpgLib         = "com.jsuereth" %% "gpg-library" % "0.8.1"
+  val gpgLib         = "com.jsuereth" %% "gpg-library" % "0.8.2"
   val javaMail       = "javax.mail" % "mail" % "1.4.7"
   val commonsLang    = "commons-lang" % "commons-lang" % "2.6"
 

311a44790895850552149c43077003762ecbb6ec
Antonio Cunei <antonio.cunei@typesafe.com>
2013-11-28 02:41:17 +0100

    Cleaning actor for build directory

--- distributed/actorProjects/src/main/scala/distributed/project/build/BuildRunnerActor.scala
+++ distributed/actorProjects/src/main/scala/distributed/project/build/BuildRunnerActor.scala
@@ -4,32 +4,41 @@ package build
 
 import model._
 import logging.Logger
-import akka.actor.Actor
+import akka.actor.{ ActorRef, Actor, Props, PoisonPill, Terminated }
 import distributed.project.resolve.ProjectResolver
 import actorpatterns.forwardingErrorsToFutures
 import java.io.File
 import distributed.repo.core._
 import sbt.IO
-import distributed.project.controller.{Controller, Controlled, Done}
+import distributed.project.controller.{ Controller, Controlled, Done }
 import sbt.Path._
 import distributed.utils.Time._
 
 case class RunBuild(build: RepeatableProjectBuild, outProjects: Seq[Project], children: Seq[BuildOutcome], log: Logger)
 
+class CleaningBuildActor extends Actor {
+  def receive = {
+    case target: File =>
+      IO.delete(target.*(sbt.DirectoryFilter).get.filter(markedForDeletion))
+      self ! PoisonPill
+  }
+}
 /** This actor can run builds locally and return the generated artifacts. */
 class BuildRunnerActor(builder: LocalBuildRunner, target: File, exp: CleanupExpirations) extends Actor {
   override def preStart() = {
     // Cleanup works in two stages; see ExtractorActor for details.
     // Note that cleanup is performed independently for the extraction and build directories
     target.*(sbt.DirectoryFilter).get.filter(upForDeletion(_, exp)).foreach(prepareForDeletion)
+    // spawn the cleaning actor
+    context.actorOf(Props(new CleaningBuildActor)) ! target
   }
-    
+
   def receive = {
-    case Controlled(RunBuild(build, outProjects, children, log),from) => 
+    case Controlled(RunBuild(build, outProjects, children, log), from) =>
       Controller.forwardingErrorsToFuturesControlled(sender, from) {
-        log info ("--== Building %s ==--" format(build.config.name))
+        log info ("--== Building %s ==--" format (build.config.name))
         sender ! Done(builder.checkCacheThenBuild(target, build, outProjects, children, log), from)
-        log info ("--== End Building %s ==--" format(build.config.name))
+        log info ("--== End Building %s ==--" format (build.config.name))
       }
-  }   
+  }
 }

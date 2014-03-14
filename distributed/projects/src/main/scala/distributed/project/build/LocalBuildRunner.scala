bcc38993c64dc76adb85b7ff4995c49a2dca9c19
Antonio Cunei <antonio.cunei@typesafe.com>
2014-03-14 18:20:20 +0100

    Remove dead comment

--- distributed/projects/src/main/scala/distributed/project/build/LocalBuildRunner.scala
+++ distributed/projects/src/main/scala/distributed/project/build/LocalBuildRunner.scala
@@ -25,7 +25,6 @@ class LocalBuildRunner(builder: BuildRunner,
     buildData: BuildData): BuildOutcome = {
     val log = buildData.log
     try {
-//(log: Logger, debug: Boolean, timestamp: String
       try {
         val subArtifactsOut = LocalRepoHelper.getPublishedDeps(build.uuid, repository, log) // will throw exception if not in cache yet
         LocalRepoHelper.debugArtifactsInfo(subArtifactsOut, log)
@@ -39,7 +38,7 @@ class LocalBuildRunner(builder: BuildRunner,
       }
     } catch {
       case t =>
-        BuildFailed(build.config.name, children, prepareLogMsg(buildData.log, t))
+        BuildFailed(build.config.name, children, prepareLogMsg(log, t))
     }
   }
 

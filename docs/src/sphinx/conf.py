c4ad89b6d3c0ce1da075bee59bb77baebbaca955
Antonio Cunei <antonio.cunei@typesafe.com>
2014-03-18 17:02:21 +0100

    Bump version to 0.8.2-SNAPSHOT

--- docs/src/sphinx/conf.py
+++ docs/src/sphinx/conf.py
@@ -8,8 +8,8 @@ extensions = ['sphinxcontrib.issuetracker', 'sphinx.ext.extlinks', 'howto']
 # Project variables
 
 project = 'dbuild'
-version = '0.8.1'
-release = '0.8.1'
+version = '0.8.2'
+release = '0.8.2-SNAPSHOT'
 sbt_version = '0.12.4'
 
 # General settings
--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object DistributedBuilderBuild extends Build with BuildHelper {
 
   override def settings = super.settings ++ SbtSupport.buildSettings
 
-  def MyVersion: String = "0.8.1"
+  def MyVersion: String = "0.8.2-SNAPSHOT"
   
   lazy val root = (
     Project("root", file(".")) 

485889447e183f5c931826ff71c4ceb7e74f5592
Antonio Cunei <antonio.cunei@typesafe.com>
2014-04-03 01:13:45 +0200

    Removed the "ProjectRepository" wrapper, essentially unused

--- distributed/repo/src/main/scala/distributed/repo/core/ProjectRepoMain.scala
+++ distributed/repo/src/main/scala/distributed/repo/core/ProjectRepoMain.scala
@@ -29,8 +29,8 @@ object ProjectRepoMain {
   // TODO - this file-specific knowledge is evil
   val cacheDir = Repository.defaultCacheDir
   val cache = Repository.default
-  val projectRepo = new ReadableProjectRepository(cache)
-  
+  private def getProjectInfo(uuid:String) = LocalRepoHelper.getProjectInfo(uuid, cache)
+
   def main(args: Array[String]): Unit = {
     args.toSeq match {
       case Seq("project", uuid) => printProjectInfo(uuid)
@@ -92,8 +92,8 @@ object ProjectRepoMain {
   }
   
   private def printProjects(uuids: Seq[String]): Unit = {
-    val meta = uuids map { id => 
-      val (info, _, _) = projectRepo getProjectInfo id
+    val meta = uuids map { uuid =>
+      val (info, _, _) = getProjectInfo(uuid)
       info
     }
     val names = padStrings(meta map (_.project.config.name))
@@ -114,14 +114,14 @@ object ProjectRepoMain {
   }
   
   def printProjectDependencies(uuid:String): Unit = {
-    val (meta, _, _) = projectRepo.getProjectInfo(uuid)
+    val (meta, _, _) = getProjectInfo(uuid)
     println(" -- Dependencies --")
     for(uuid <- meta.project.depInfo flatMap (_.dependencyUUIDs))
       println("    * " + uuid)
   }
   
   def printProjectArtifacts(uuid:String): Unit = {
-    val (meta, _, arts) = projectRepo.getProjectInfo(uuid)
+    val (meta, _, arts) = getProjectInfo(uuid)
     println(" -- Artifacts -- ")
     val groups = padStrings(arts map (_.info.organization))
     val names = padStrings(arts map (_.info.name))
@@ -143,7 +143,7 @@ object ProjectRepoMain {
   }
   
   def printProjectFiles(uuid: String): Unit = {
-    val (_, artFiles,arts) = projectRepo.getProjectInfo(uuid)
+    val (_, artFiles,arts) = getProjectInfo(uuid)
     println(" -- Files -- ")
     
     val groups = artFiles groupBy { x => x._2.location.take(x._2.location.lastIndexOf('/')) }

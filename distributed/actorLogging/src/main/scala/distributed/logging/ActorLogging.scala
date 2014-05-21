ae3ec660e2a2150b4fa3158a444e0bf6f3887290
Antonio Cunei <antonio.cunei@typesafe.com>
2014-05-21 19:52:46 +0200

    Print the project name while logging
    Especially in long builds, it is always tricky to find the portion
    corresponding to a specific project in the log. This commit adds
    the project name to the log messages, making it much easier to
    scan the log for information. Note: this may possibly break
    those scripts that manually parse the log messages.

--- distributed/actorLogging/src/main/scala/distributed/logging/ActorLogging.scala
+++ distributed/actorLogging/src/main/scala/distributed/logging/ActorLogging.scala
@@ -1,9 +1,9 @@
 package distributed
 package logging
 
-import akka.actor.{ActorRef, Actor, Props, PoisonPill, Terminated}
-import sbt.{ 
-  Level, 
+import akka.actor.{ ActorRef, Actor, Props, PoisonPill, Terminated }
+import sbt.{
+  Level,
   LogEvent,
   Success,
   Log,
@@ -16,25 +16,24 @@ import sbt.{
 }
 
 /** A command to the logging system to log a given event into a particular path. */
-case class LogCmd(path: String, evt: LogEvent)
-
+case class LogCmd(path: String, evt: LogEvent, projName: String)
 
 /** A logger that sends LogCmds to an actor rather than doing anything directly. */
-class ActorLogger(nested: ActorRef, path: String = "root") extends Logger {
+class ActorLogger(nested: ActorRef, path: String = "root", val projName: String = "") extends Logger {
   import Level._
   // TODO - Keep a reference of what's enabled locally...
-  def newNestedLogger(name: String): Logger = {
-    new ActorLogger(nested, path + "/" + name)
+  def newNestedLogger(name: String, projName: String = ""): Logger = {
+    new ActorLogger(nested, path + "/" + name, projName)
   }
-  def trace(t: => Throwable): Unit = 
+  def trace(t: => Throwable): Unit =
     sendLogEvent(new Trace(t))
   def success(msg: => String): Unit =
     sendLog(Level.Info, msg)
-  def buffer[T](t: => T): T = t  // TODO - buffer actor messages....
+  def buffer[T](t: => T): T = t // TODO - buffer actor messages....
   def err(s: => String): Unit =
     sendLog(Error, s)
   def out(s: => String): Unit =
-    sendLog(Info, s)  
+    sendLog(Info, s)
   def control(event: sbt.ControlEvent.Value, msg: => String): Unit =
     sendLogEvent(new ControlEvent(event, msg))
   def logAll(events: Seq[sbt.LogEvent]): Unit = {
@@ -42,10 +41,10 @@ class ActorLogger(nested: ActorRef, path: String = "root") extends Logger {
   }
   def log(level: sbt.Level.Value, msg: => String): Unit =
     sendLog(level, msg)
-  private def sendLog(level: Level.Value, msg: => String): Unit = 
+  private def sendLog(level: Level.Value, msg: => String): Unit =
     sendLogEvent(new Log(level, msg))
   private def sendLogEvent(event: LogEvent): Unit =
-    nested ! LogCmd(path, event)
+    nested ! LogCmd(path, event, projName)
 }
 
 object ActorLogHelper {
@@ -59,7 +58,7 @@ class LogDirManagerActor(logDir: java.io.File) extends Actor {
 
   def receive = {
     case log: LogCmd =>
-      findOrCreateLogger(log.path) ! log
+      findOrCreateLogger(log.path, log.projName) ! log
     case "exit" =>
       loggers.values.foreach { _ ! PoisonPill }
     case Terminated(p) =>
@@ -72,7 +71,7 @@ class LogDirManagerActor(logDir: java.io.File) extends Actor {
       }
   }
 
-  def findOrCreateLogger(path: String): ActorRef =
+  def findOrCreateLogger(path: String, name: String): ActorRef =
     loggers get path getOrElse {
       val logger = context.actorOf(Props(new LoggerFileWriteActor(logDir, path)), "Logger-" + ActorLogHelper.cleanPath(path))
       context.watch(logger)
@@ -83,20 +82,29 @@ class LogDirManagerActor(logDir: java.io.File) extends Actor {
 
 /** Helper for logger implementations to write out LogCmds. */
 trait LogToOutput {
-  def log(l: LogCmd): Unit = l.evt match {
+  def log(lo: LogCmd): Unit = lo.evt match {
     case t: Trace =>
       // TODO - Real trace levels...
       writeLog(sbt.StackTrace.trimmed(t.exception, 1))
     case l: Log if l.level >= level =>
       val sb = new StringBuffer()
-      for(line <- l.msg.split("""\n"""")) {
-        sb append ("[") append (l.level.toString) append("] ")
+      for (line <- l.msg.split("""\n"""")) {
+        // if projName, we are more interested in the project name,
+        // rather than the error level (unless it is something other than "info")
+        if (lo.projName != "" && l.level == sbt.Level.Info)
+          sb append ("[") append (lo.projName) append "] "
+        else {
+          sb append ("[")
+          if (lo.projName != "")
+            sb append (lo.projName) append (":")
+          sb append (l.level.toString) append ("] ")
+        }
         sb append line append "\n"
       }
       writeLog(sb.toString)
-    case _ => ()  // Ignore all else...
-  } 
-  
+    case _ => () // Ignore all else...
+  }
+
   def writeLog(in: String): Unit
   def level: sbt.Level.Value
 }
@@ -110,64 +118,62 @@ class LoggerFileWriteActor(logDir: java.io.File, path: String) extends Actor wit
     sbt.IO touch f
     new java.io.FileWriter(f)
   }
-  var level: sbt.Level.Value = Level.Debug  
+  var level: sbt.Level.Value = Level.Debug
   var output = newWriter
-  
+
   override def postStop() = output.close()
-  
+
   override def preStart() = {
-    output.write("------ LOG - "+path+" --------\n")
+    output.write("------ LOG - " + path + " --------\n")
   }
-  
+
   override def preRestart(r: Throwable, msg: Option[Any]): Unit = {
     // TODO - just check for IO exception that's not closing
     try output.close catch { case _: java.io.IOException => () }
     output = newWriter
     output.write("Restarting log after " + msg + "!\n")
     output.write(r.getMessage)
-    output.write("\n------ "+path+" --------\n")
-  } 
-  
+    output.write("\n------ " + path + " --------\n")
+  }
+
   def receive = {
     //case l: LogCmd if l.path != path =>
-      // Make new logger for that path and forward messages to it...
-    case l : LogCmd =>
+    // Make new logger for that path and forward messages to it...
+    case l: LogCmd =>
       // TODO - Check paths...
       log(l)
   }
-    
-   def writeLog(msg: String): Unit = {
-     output write msg
-     output.flush()
-   }
+
+  def writeLog(msg: String): Unit = {
+    output write msg
+    output.flush()
+  }
 }
-
-
 
 /** An actor that takes LogCmd messages and writes them to a file. */
 class SystemOutLoggerActor(debug: Boolean) extends Actor with LogToOutput {
   import sbt.Path._
-  val output = System.out  
+  val output = System.out
   val level = if (debug) Level.Debug else Level.Info
-  
+
   def receive = {
-    case l : LogCmd => log(l)
+    case l: LogCmd => log(l)
     case "exit" =>
       context.stop(self)
   }
-    
-   def writeLog(msg: String): Unit = {
-     output print msg
-     output.flush()
-   }
+
+  def writeLog(msg: String): Unit = {
+    output print msg
+    output.flush()
+  }
 }
 
 /** An actor that chains to other loggers. */
 class ChainedLoggerSupervisorActor extends Actor {
   var loggers: Seq[ActorRef] = List.empty
-  var terminationSender:ActorRef = self
+  var terminationSender: ActorRef = self
   def receive = {
-    case p: Props => 
+    case p: Props =>
       val logger = context actorOf p
       context.watch(logger)
       loggers = logger +: loggers

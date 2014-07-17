36d5c8c5dce3877b0eb931dac2ec1854a4a6f5f0
Antonio Cunei <antonio.cunei@typesafe.com>
2014-07-17 22:37:51 +0200

    Update description of index case classes

--- distributed/metadata/src/main/scala/com/typesafe/reactiveplatform/manifest/Manifest.scala
+++ distributed/metadata/src/main/scala/com/typesafe/reactiveplatform/manifest/Manifest.scala
@@ -3,5 +3,9 @@ package com.typesafe.reactiveplatform.manifest
 /**
 * Represents a manifest of all information included in a
 * typesafe-reactive-platform build.
+* @param trp Information about the platform, such as end of life, release date, etc.
+* @param modules Information about modules included in the platofrm, including all
+* necessary information to discover the module and force a revision
+* in the user build.
 */
 case class Manifest(trp: PlatformInfo, modules: Seq[ModuleInfo])

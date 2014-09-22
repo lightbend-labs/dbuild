149810f0a93e1b0ad94a7c07ef4366248aa8f1b2
Antonio Cunei <antonio.cunei@typesafe.com>
2014-09-22 02:19:41 +0200

    Bump version to 0.9.2

--- docs/src/sphinx/conf.py
+++ docs/src/sphinx/conf.py
@@ -9,7 +9,7 @@ extensions = ['sphinxcontrib.issuetracker', 'sphinx.ext.extlinks', 'howto']
 
 project = 'dbuild'
 version = '0.9.2'
-release = '0.9.2-SNAPSHOT'
+release = '0.9.2'
 sbt_version = '0.12.4'
 
 # General settings

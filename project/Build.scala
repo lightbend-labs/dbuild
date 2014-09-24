586b79a4234fb38460386a5cea9add94533f6f46
Antonio Cunei <antonio.cunei@typesafe.com>
2014-09-24 17:27:19 +0200

    Bump version to 0.9.3-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object DBuilderBuild extends Build with BuildHelper {
 
   override def settings = super.settings ++ SbtSupport.buildSettings
 
-  def MyVersion: String = "0.9.2"
+  def MyVersion: String = "0.9.3-SNAPSHOT"
   
   lazy val root = (
     Proj("root") 

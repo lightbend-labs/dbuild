52d8bbddbfe33f4b38b524b3708fe173842a21ae
Antonio Cunei <antonio.cunei@typesafe.com>
2016-10-13 03:25:51 +0200

    missing file

---
+++ project/RemoteDepHelper.scala
@@ -0,0 +1,18 @@
+import sbt._
+import Keys._
+import Dependencies._
+
+// DSL for adding remote deps like local deps.
+class RemoteDepHelper(p: Project) {
+  def dependsOnRemote(ms: ModuleID*): Project = p.settings(libraryDependencies ++= ms)
+  def dependsOnSbt(ms: ((String,String)=>ModuleID)*): Project = p.settings(libraryDependencies <++= (scalaVersion) {sv => ms map {_(RemoteDepHelper.sbtVer(sv), sv)}})
+  def dependsOnAkka(): Project = p.settings(libraryDependencies <+= (scalaVersion) {sv => if (sv.startsWith("2.9")) akkaActor29 else
+    if (sv.startsWith("2.10")) akkaActor210 else akkaActor211})
+}
+object RemoteDepHelper {
+  implicit def p2remote(p: Project): RemoteDepHelper = new RemoteDepHelper(p)
+
+  def sbtVer(scalaVersion:String) = if (scalaVersion.startsWith("2.9")) sbtVersion12 else
+    if(scalaVersion.startsWith("2.10")) sbtVersion13 else sbtVersion100
+}
+
